9491b160a75b3352da697c5ccc67f41679b3c4eb
VadimZb <vvz.dev@gmail.com>
2021-04-14 20:07:20 +0300

    👌 IMPROVE: HeaderCard aligned to right

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="home">
-  	<HeaderCard />
-  </div>
+    <div class="home">
+        <HeaderCard />
+    </div>
 </template>
 
 <script>
-import HeaderCard from "../components/HeaderCard.vue"
+import HeaderCard from "../components/HeaderCard.vue";
 export default {
-  name: 'Home',
-  components: {
-  	HeaderCard
-  }
-}
+    name: "Home",
+    components: {
+        HeaderCard,
+    },
+};
 </script>
