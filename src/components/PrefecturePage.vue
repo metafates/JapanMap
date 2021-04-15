7884c03c90b3ddbd462e2364fbb830e45aa5fe5e
VadimZb <vvz.dev@gmail.com>
2021-04-15 23:00:31 +0300

    📦 NEW: Full page added

---
+++ src/components/PrefecturePage.vue
@@ -0,0 +1,127 @@
+<template>
+    <v-dialog
+        v-model="dialog"
+        fullscreen
+        hide-overlay
+        transition="dialog-bottom-transition"
+    >
+        <v-card>
+            <v-toolbar>
+                <v-btn icon @click="dialog = false">
+                    <v-icon>mdi-close</v-icon>
+                </v-btn>
+            </v-toolbar>
+            <v-card-text class="mt-10 limitedWidth mx-auto text-center">
+                <p class="text-h3 text--primary text-center font-weight-bold">
+                    {{ p.name }}
+                </p>
+                <p class="text-h5 text-center">
+                    {{ p.preview.intro }}
+                </p>
+                <v-row justify="center" class="my-10">
+                    <div class="d-flex" v-for="(field, i) in fields" :key="i">
+                        <v-tooltip top>
+                            <template v-slot:activator="{ on, attrs }">
+                                <div
+                                    v-on="on"
+                                    v-bin="attrs"
+                                    class="d-flex flex-column mx-10 my-5 align-center"
+                                >
+                                    <v-icon x-large>{{ field.icon }}</v-icon>
+                                    <span class="text-h6 mt-3">{{
+                                        field.value
+                                    }}</span>
+                                </div>
+                            </template>
+                            <span>{{ field.name }}</span>
+                        </v-tooltip>
+                    </div>
+                </v-row>
+                <p
+                    class="text-h5"
+                    v-for="(paragraph, i) in p.main"
+                    :key="'A' + i"
+                >
+                    {{ paragraph }}
+                </p>
+                <v-img
+                    :src="p.preview.image.large"
+                    class="rounded-xl mx-auto my-10"
+                    width="700"
+                ></v-img>
+                <div
+                    class="paragraph"
+                    v-for="(listItem, i) in p.list"
+                    :key="'B' + i"
+                >
+                    <p class="text-h4 mb-10 font-weight-bold">
+                        {{ listItem.title }}
+                    </p>
+                    <p
+                        v-for="(paragraph, i) in listItem.description"
+                        :key="'C' + i"
+                        class="text-h5"
+                    >
+                        {{ paragraph }}
+                    </p>
+                    <v-img
+                        :src="listItem.image"
+                        class="rounded-xl mx-auto my-10"
+                        width="700"
+                    ></v-img>
+                </div>
+            </v-card-text>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    props: {
+        dialog: {
+            type: Boolean,
+            default: false,
+        },
+        prefecture: {
+            type: String,
+            default: "JP-01",
+        },
+    },
+    data() {
+        return {
+            p: this.$store.state.prefectures[this.prefecture],
+        };
+    },
+    computed: {
+        fields() {
+            // TODO: Make toolbar to display field name
+            return [
+                {
+                    name: "Блюдо",
+                    icon: "$food",
+                    value: this.p.preview.food,
+                },
+                {
+                    name: "Культура",
+                    icon: "$sightseeings",
+                    value: this.p.preview.culture,
+                },
+                {
+                    name: "Диалект",
+                    icon: "$language",
+                    value: this.p.preview.dialect,
+                },
+            ];
+        },
+    },
+};
+</script>
+
+<style lang="stylus" scoped>
+.paragraph
+    margin-top: 10rem
+    margin-bottom: 10rem
+
+.limitedWidth
+    max-width: 1000px
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,22 @@
 <template>
-    <div class="home">
+    <v-container fluid>
         <PreviewCard :prefecture="prefecture" />
-    </div>
+        <PrefecturePage :prefecture="prefecture" :dialog="true" />
+    </v-container>
 </template>
 
 <script>
 // Preview Card
-import PreviewCard from "../components/PreviewCard.vue";
+import PreviewCard from "../components/PreviewCard";
+
+// Prefecture Page
+import PrefecturePage from "../components/PrefecturePage";
 
 export default {
     name: "Home",
     components: {
         PreviewCard,
+        PrefecturePage,
     },
     data() {
         return {
