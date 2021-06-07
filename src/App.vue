28b9d9a1281dfe44aa4678d7515f8aad5e60c254
VadimZb <vvz.dev@gmail.com>
2021-06-07 17:03:47 +0300

    🐛 FIX:

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,3 @@
         </v-main>
     </v-app>
 </template>
-
-<script>
-import prefectures from "./prefectures/prefectures.json";
-export default {
-    name: "App",
-    created() {
-        this.$store.commit("setup", Object.freeze(prefectures));
-    },
-};
-</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import prefectures from "../prefectures/prefectures.json";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        prefectures: {},
+        prefectures: Object.freeze(prefectures),
     },
-    mutations: {
-        setup(state, prefectures) {
-            state.prefectures = prefectures;
-        },
-    },
+    mutations: {},
     actions: {},
     modules: {},
 });
